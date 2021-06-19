381d7b78a6a51b68d41a849e4a6c4bb1935d83d3
devPremkumar <rameshpremkumar@outlook.com>
2021-06-19 11:13:43 +0530

    Project added

---
+++ Delivery-master/config/mainconfig.js
@@ -0,0 +1,25 @@
+export const MainConfig = {
+    AppDetails: {
+        app_name: "Couriersy", 
+        app_description: "Couriersy fast deliver app",
+        app_identifier: "com.devpoints.Couriersy",
+        ios_app_version: "1.0.0", 
+        android_app_version: 100
+    },
+    FirebaseConfig: {
+        apiKey: "AIzaSyDA2EhcjKalqsEZLxq3jYiPrctbHlc0Z9A",
+        authDomain: "couriersy.firebaseapp.com",
+        databaseURL: "https://couriersy-default-rtdb.firebaseio.com",
+        projectId: "couriersy",
+        storageBucket: "couriersy.appspot.com",
+        messagingSenderId: "1076815273264",
+        appId: "1:1076815273264:web:82863b7d91c3d5d61bbcf5",
+        measurementId: "G-FGNF2946DY"
+    },
+    Google_Map_Key: "AIzaSyAcMjV7M_963gb7gHt1chnK01I_wH_qGl8",
+    facebookAppId: "196906895540591",
+    PurchaseDetails: {
+        CodeCanyon_Purchase_Code: "",
+        Buyer_Email_Address: ""
+    }
+}
